2a2327b1d615003093d176f20d5b1b48c42a1546
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-06-03 17:53:10 -0700

    Added state for MessageList, used props to render Message

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import logo from './logo.svg';
 // import './App.css';
 
 class MessageList extends React.Component {
-
-  render() {
+  constructor(props) {
+    super(props);
     const msgs = this.props.messages;
     const listMessages = msgs.map((msg) => {
       return <Message key={msg.id} value={msg.text} />;
         }
       );
+    this.state = {value: listMessages}
+    };
+  render() {
       return(
         <div>
           <h2>Message List</h2>
-          <ul>{listMessages}</ul>
+          <ul>{this.state.value}</ul>
         </div>
       );
     }
@@ -21,7 +24,41 @@ class MessageList extends React.Component {
 
 class Message extends React.Component {
   render() {
-      return <li key={this.props.id}>{this.props.value}</li>;
+      return <li>{this.props.value}</li>;
+  }
+}
+
+class TextArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value:'',
+      defaultValue: 'Write your message here...'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  // adds message to <ul> message list
+  handleSubmit(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+      return <form>
+        <h3 id="instruction">Compose Message</h3>
+        <textarea
+          id="message"
+          onChange={this.handleChange}
+          placeholder={this.state.defaultValue}>
+        </textarea>
+        <br></br>
+        <button onClick={this.handleSubmit}>submit</button>
+      </form>;
   }
 }
 
@@ -30,7 +67,7 @@ class App extends Component {
     const initialMessages = [
     {id: 1, text: "This is the first post"},
     {id: 2, text: "This is the second post"},
-    {id: 2, text: "This is the third post"}
+    {id: 3, text: "This is the third post"}
   ];
   // <img src={logo} className="App-logo" alt="logo" />
     return (
@@ -39,9 +76,9 @@ class App extends Component {
           <h2>Welcome to the Assignment 2<br></br>Message Board</h2>
 
         </div>
-        <p className="App-intro">
-          Write your message below:
-        </p>
+        <div id="container-div">
+          <TextArea/>
+        </div>
         <div className="Messages">
             <MessageList messages={initialMessages}/>
         </div>
